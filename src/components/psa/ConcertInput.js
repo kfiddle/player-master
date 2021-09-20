0ce95b187fd1640638bf447d1b17b7f1a666e999
kfiddle <kenjfiddle@gmail.com>
2021-09-19 21:47:23 -0400

    various

--- src/components/psa/ConcertInput.js
+++ src/components/psa/ConcertInput.js
@@ -1,17 +1,46 @@
+import { useState } from "react";
+
+import React from "react";
+
 import styles from "./ConcertInput.module.css";
 
 const ConcertInput = (props) => {
+  const [selectedState, setSelectedState] = useState([false, false]);
+
+  const sendUpAnswerState = (whichInput) => {
+    whichInput === "accept"
+      ? setSelectedState((previous) => [!previous[0], false])
+      : setSelectedState((previous) => [false, !previous[1]]);
+
+    props.setInputState(props.performance, selectedState);
+    console.log("this one says " + selectedState);
+  };
+
   return (
     <div className={styles.outerContainer}>
-      <h2 className={styles.title}>{props.title}</h2>
+      <h2 className={styles.title}>{props.label}</h2>
       <div className={styles.radioButtonsDiv}>
         <div>
           <label className={styles.buttonLabel}>Accept</label>
-          <input type="radio" />
+
+          <input
+            type="radio"
+            checked={selectedState[0]}
+            onChange={() => {
+              sendUpAnswerState("accept");
+            }}
+          />
         </div>
+
         <div>
           <label className={styles.buttonLabel}>Decline</label>
-          <input type="radio" />
+          <input
+            type="radio"
+            checked={selectedState[1]}
+            onChange={ () => {
+              sendUpAnswerState("decline");
+            }}
+          />
         </div>
       </div>
     </div>
--- src/components/psa/PSA.js
+++ src/components/psa/PSA.js
@@ -1,7 +1,50 @@
+import { useRef, useEffect, useState } from "react";
+
+import GetAList from "../helperFunctions/GetAList";
 import ConcertInput from "./ConcertInput";
 import styles from "./PSA.module.css";
 
 const PSA = (props) => {
+  const [gigsList, setGigsList] = useState([]);
+  const [gigsWithAnswers, setGigsWithAnswers] = useState([]);
+
+  useEffect(async () => {
+    const gigsResponseList = await GetAList("get-all-performances");
+    let listToFill = [];
+    for (let performance of gigsResponseList) {
+      let answerState = [];
+      listToFill.push({ performance, answerState });
+    }
+
+    setGigsWithAnswers(listToFill);
+    setGigsList(gigsResponseList);
+  }, []);
+
+  const putStateInList = (performance, state) => {
+    console.log(performance, state)
+    let tempList = gigsWithAnswers;
+    for (let gigAndAnswer of tempList) {
+      if (gigAndAnswer.performance === performance) {
+        gigAndAnswer.answerState = state;
+      }
+    }
+    setGigsWithAnswers(tempList);
+  };
+
+  const displayableConcerts = gigsWithAnswers.map((gigAnswer) => (
+    <ConcertInput
+      performance={gigAnswer.performance}
+      label={gigAnswer.performance.title}
+      key={gigAnswer.performance.id}
+      setInputState={putStateInList}
+    />
+  ));
+
+  const showAnswers = (event) => {
+    event.preventDefault();
+    console.log(gigsWithAnswers);
+  };
+
   return (
     <div className={styles.outerContainer}>
       <div className={styles.textDiv}>
@@ -9,18 +52,16 @@ const PSA = (props) => {
           Dear so-and-so, this form will serve as your Personal Service
           Agreement, (PSA). Below is a listing of all the services you are being
           offered by the Erie Philharmonic for the 2022-23 season, as of April
-          22, 2022.
-        </p>
-        As per the
+          22, 2022. As per the </p>
         <h2 className={styles.masterAgreement}>
           <a>Master Agreement</a>
-        </h2>
+        </h2> <p>
         , Article III(d), you must accept or decline these services no later
         than June 30th of this year. Please check one of the boxes next to EACH
         service listed below, indicating whether you intend to accept or decline
         it. Once each service has been marked, click Submit to enter your
         responses. Although you will be able to view your responses, you will
-        not be able to amend this form after June 30th.
+        not be able to amend this form after June 30th. </p>
         <p>
           For the 2022-23 season, the Erie Philharmonic is currently able to
           offer you a total of 46 services at the position of Concertmaster.
@@ -35,14 +76,17 @@ const PSA = (props) => {
       </div>
 
       <form className={styles.psaForm}>
-        <ConcertInput title="Pops Uno"/>
-        <ConcertInput title="Messiah"/>
-        <ConcertInput title="Sym 1"/>
-        <ConcertInput title="Pops 2"/>
-        <ConcertInput title="Beat Beethoven"/>
+        {/* <ConcertInput title="Pops Uno" />
+        <ConcertInput title="Messiah" />
+        <ConcertInput title="Sym 1" />
+        <ConcertInput title="Pops 2" />
+        <ConcertInput title="Beat Beethoven" /> */}
 
+        {displayableConcerts}
 
-        <button className={styles.submitButton}>Submit</button>
+        <button className={styles.submitButton} onClick={showAnswers}>
+          Submit
+        </button>
       </form>
     </div>
   );
